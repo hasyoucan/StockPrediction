2b6fe8041caa249bebfd3f7d68159aa93ae7e300
Sakai Takao <s.takao.73@gmail.com>
2018-06-16 17:50:23 +0900

    4分類で予測するように変更。

--- LSTM4/StockPrediction.py
+++ LSTM4/StockPrediction.py
@@ -4,7 +4,6 @@ import math
 import numpy as np
 import pandas as pd
 
-import Technical
 from MultiLoader import MultiLoader
 
 from keras.models import Sequential
@@ -14,14 +13,15 @@ from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 
 from sklearn import metrics
-# from sklearn import cross_validation as cv
 
 
-hidden_neurons = 128
-training_days = 25
-prediction_days = 2
+hidden_neurons = 400
+training_days = 75
 
 epochs = 50
+
+threshold = 0.01
+category_threshold = [-1, -threshold, 0, threshold, 1]
 
 
 def load_data(date_file, stock_data_files, target_stock_name):
@@ -31,13 +31,10 @@ def load_data(date_file, stock_data_files, target_stock_name):
     """
     multi_loader = MultiLoader(date_file, stock_data_files)
 
-    high = multi_loader.extract('adj_high')
-    low = multi_loader.extract('adj_low')
-    end = multi_loader.extract('end')
     adj_ends = multi_loader.extract('adj_end')
     adj_starts = multi_loader.extract('adj_start')
 
-    return (high, low, end, adj_starts, adj_ends)
+    return (adj_starts, adj_ends)
 
 
 def rate_of_decline(values):
@@ -48,6 +45,12 @@ def rate_of_decline(values):
     # 最初は 1 なのです。
     ret_index[0] = 1.0
     return ret_index
+
+
+def pct_change(values):
+    returns = pd.Series(values).pct_change()
+    returns[0] = 0
+    return returns
 
 
 def log_diff(values):
@@ -62,107 +65,27 @@ def log_diff(values):
 
 
 def convert_data(values):
-    return rate_of_decline(values)
+    return log_diff(values)
 
 
-def create_train_data(high, low, end, adj_start, adj_ends, y_data, samples):
+def create_train_data(adj_start, adj_ends, y_data, samples):
 
-    udr_start = np.asarray([convert_data(v) for v in adj_start])
-    udr_high = np.asarray([convert_data(v) for v in low])
-    udr_low = np.asarray([convert_data(v) for v in high])
+    # udr_start = np.asarray([convert_data(v) for v in adj_start])
     udr_end = np.asarray([convert_data(v) for v in adj_ends])
-
-    # tech_period = {
-    #     'ma_short': 25,
-    #     'ma_long': 75,
-
-    #     'madr_short': 5,
-    #     'madr_long': 25,
-
-    #     'macd_short': 12,
-    #     'macd_long': 26,
-    #     'macd_signal': 9,
-
-    #     'rsi': 14,
-    #     'roc': 12,
-
-    #     'fast_stoc_k': 5,
-    #     'fast_stoc_d': 3,
-
-    #     'slow_stoc_k': 15,
-    #     'slow_stoc_d': 3,
-    #     'slow_stoc_sd': 3,
-    # }
-
-    # ma_short = np.asarray([Technical.moving_average(
-    #     v, tech_period['ma_short'])[0].values for v in udr_end])
-    # ma_long = np.asarray([Technical.moving_average(
-    #     v, tech_period['ma_long'])[0].values for v in udr_end])
-    # madr_short = np.asarray([Technical.moving_average_deviation_rate(
-    #     v, tech_period['madr_short'])[0].values for v in adj_ends])
-    # madr_long = np.asarray([Technical.moving_average_deviation_rate(
-    #     v, tech_period['madr_long'])[0].values for v in adj_ends])
-    # macd = np.asarray([Technical.macd(v,
-    #                                   tech_period['macd_short'],
-    #                                   tech_period['macd_long'],
-    #                                   tech_period['macd_signal'])[0][0].values for v in adj_ends])
-    # macd_signal = np.asarray([Technical.macd(v,
-    #                                          tech_period['macd_short'],
-    #                                          tech_period['macd_long'],
-    #                                          tech_period['macd_signal'])[1][0].values for v in adj_ends])
-    # rsi = np.asarray([Technical.rsi(v, tech_period['rsi'])[
-    #                  0].values for v in adj_ends])
-    # roc = np.asarray([Technical.roc(v, tech_period['roc'])[
-    #                  0].values for v in adj_ends])
-
-    # fast_stoc_k = []
-    # fast_stoc_d = []
-    # slow_stoc_d = []
-    # slow_stoc_sd = []
-    # for i, e in enumerate(end):
-    #     h = high[i]
-    #     l = low[i]
-    #     fast_stoc_k.append(Technical.stochastic_K(
-    #         e, h, l, tech_period['fast_stoc_k'])[0].values)
-    #     fast_stoc_d.append(Technical.stochastic_D(e, h, l,
-    #                                               tech_period['fast_stoc_k'],
-    #                                               tech_period['fast_stoc_d'])[0].values)
-    #     slow_stoc_d.append(Technical.stochastic_D(e, h, l,
-    #                                               tech_period['slow_stoc_k'],
-    #                                               tech_period['slow_stoc_d'])[0].values)
-    #     slow_stoc_sd.append(Technical.stochastic_slowD(e, h, l,
-    #                                                    tech_period['slow_stoc_k'],
-    #                                                    tech_period['slow_stoc_d'],
-    #                                                    tech_period['slow_stoc_sd'])[0].values)
-    # fast_stoc_k = np.asarray(fast_stoc_k)
-    # fast_stoc_d = np.asarray(fast_stoc_d)
-    # slow_stoc_d = np.asarray(slow_stoc_d)
-    # slow_stoc_sd = np.asarray(slow_stoc_sd)
-
-    # 先頭のこの日数分のデータは捨てる
-    # chop = max(tech_period.values())
-    chop = 0
 
     # 銘柄×日付→日付×銘柄に変換
     transposed = udr_end.transpose()
-    # transposed = np.concatenate((udr_start, udr_end)).transpose()
-    # transposed = rsi.transpose()
-    # transposed = roc.transpose()
-    # transposed = np.concatenate((macd, macd_signal)).transpose()
-    # transposed = np.concatenate((ma_short, ma_long)).transpose()
-    # transposed = np.concatenate((madr_short, madr_long)).transpose()
-    # transposed = fast_stoc_k.transpose()
-    # transposed = np.concatenate((fast_stoc_k, fast_stoc_d)).transpose()
-    # transposed = np.concatenate((slow_stoc_d, slow_stoc_sd)).transpose()
 
     _x = []
     _y = []
     # サンプルのデータを学習、 1 サンプルずつ後ろにずらしていく
     length = len(udr_end[0])
-    for i in np.arange(chop, length - samples - prediction_days + 1):
+    for i in np.arange(0, length - samples):
         s = i + samples  # samplesサンプル間の変化を素性にする
         _x.append(transposed[i:s])
-        _y.append(y_data[s:s+prediction_days])
+        __y = [0, 0, 0, 0]
+        __y[y_data[s]] = 1
+        _y.append(__y)
 
     # 上げ下げの結果と教師データのセットを返す
     return np.array(_x), np.array(_y)
@@ -171,51 +94,62 @@ def create_train_data(high, low, end, adj_start, adj_ends, y_data, samples):
 def create_model(dimension):
     model = Sequential()
     model.add(LSTM(hidden_neurons,
+                   activation='tanh',
+                   recurrent_activation='hard_sigmoid',
+                   use_bias=True,
                    kernel_initializer='random_uniform',
+                   bias_initializer='zeros',
+                   dropout=0.5,
+                   recurrent_dropout=0.5,
                    return_sequences=False,
                    batch_input_shape=(None, training_days, dimension)))
-    # model.add(Dropout(0.5))
-    model.add(Dense(prediction_days, kernel_initializer='random_uniform'))
-    model.add(Activation("linear"))
-    optimizer = Adam()
-    model.compile(loss="mean_squared_error", optimizer=optimizer)
+    model.add(Dropout(0.5))
+    model.add(Dense(4,
+                    kernel_initializer='random_uniform',
+                    bias_initializer='zeros'))
+    model.add(Activation("softmax"))
+    model.compile(loss="categorical_crossentropy",
+                  optimizer="RMSprop", metrics=['categorical_accuracy'])
 
     return model
 
 
 def print_train_history(history):
-    print("Epoch,Loss,Val loss")
+    print("Epoch, Loss, Val loss, Acc, Val Acc")
     for i in range(len(history.history['loss'])):
         loss = history.history['loss'][i]
         val_loss = history.history['val_loss'][i]
-        print("%d,%f,%f" % (i, loss, val_loss))
+        acc = history.history['categorical_accuracy'][i]
+        val_acc = history.history['val_categorical_accuracy'][i]
+        print("%d,%f,%f,%f,%f" % (i, loss, val_loss, acc, val_acc))
 
 
-def print_predict_result(preds, test_y, initial_value):
-    print("i,predict,test")
-    for i in range(0, len(preds), prediction_days):
-        for j in range(prediction_days):
-            predict = preds[i][j] * initial_value
-            test    = test_y[i][j] * initial_value
-            print("%d,%f,%f" % (i+j, predict, test))
+def print_predict_result(preds, test_y):
+    # print("i,predict,,,,actual,,,")
+    # print("i,0,1,2,3,0,1,2,3")
+    # for i in range(0, len(preds)):
+    #     predict = preds[i]
+    #     test    = test_y[i]
+    #     print("%d, %f,%f,%f,%f, %f,%f,%f,%f" % (i, predict[0], predict[1], predict[2], predict[3],
+    #                                             test[0], test[1], test[2], test[3]))
 
     tp = 0
     fp = 0
     tn = 0
     fn = 0
     for i in range(len(preds)):
-        predict = preds[i]
-        test    = test_y[i]
-        updown_p = predict[1] - predict[0]
-        updown_t = test[1] - test[0]
-        if updown_p > 0 and updown_t > 0:
+        predict = np.argmax(preds[i])
+        test    = np.argmax(test_y[i])
+        positive = True if predict == 2 or predict == 3 else False
+        true     = True if test == 2 or test == 3 else False
+        if true and positive:
             tp += 1
-        elif updown_p > 0 and updown_t <= 0:
+        if not true and positive:
             fp += 1
-        elif updown_p <= 0 and updown_t <= 0:
-            fn += 1
-        elif updown_p <= 0 and updown_t > 0:
+        if true and not positive:
             tn += 1
+        if not true and not positive:
+            fn += 1
 
     print("TP = %d, FP = %d, TN = %d, FN = %d" % (tp, fp, tn, fn))
     precision = tp / (tp + fp)
@@ -228,16 +162,16 @@ if __name__ == '__main__':
 
     target_stock = ',6501.txt'
     stock_data_files = [
-        ',Nikkei225.txt', target_stock,
+        ',Nikkei225.txt', ',TOPIX.txt', ',usdjpy.txt', target_stock,
     ]
     date_file = ',date.txt'
 
-    high, low, end, adj_starts, adj_ends = load_data(date_file, stock_data_files, target_stock)
-    y_data = convert_data(adj_starts[stock_data_files.index(target_stock)])
+    adj_starts, adj_ends = load_data(date_file, stock_data_files, target_stock)
+    y_data = pct_change(adj_starts[stock_data_files.index(target_stock)])
+    y_data = pd.cut(y_data, category_threshold, labels=False)
 
     # 学習データを生成
-    X, Y = create_train_data(high, low, end, adj_starts, adj_ends,
-                             y_data, training_days)
+    X, Y = create_train_data(adj_starts, adj_ends, y_data, training_days)
 
     # データを学習用と検証用に分割
     split_pos = int(len(X) * 0.8)
@@ -249,7 +183,7 @@ if __name__ == '__main__':
     # LSTM モデルを作成
     dimension = len(X[0][0])
     model = create_model(dimension)
-    es = EarlyStopping(monitor='loss', patience=10, verbose=1)
+    es = EarlyStopping(patience=10, verbose=1)
     history = model.fit(train_x, train_y, batch_size=10,
                         epochs=epochs, verbose=1, validation_split=0.2, callbacks=[es])
 
@@ -261,6 +195,5 @@ if __name__ == '__main__':
     print("score:", score)
 
     # 検証(2)
-    initial_value = adj_starts[stock_data_files.index(target_stock)][0]
     preds = model.predict(test_x)
-    print_predict_result(preds, test_y, initial_value)
+    print_predict_result(preds, test_y)
